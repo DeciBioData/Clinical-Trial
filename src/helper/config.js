85c77ec51f9a6fca6d0a370e9323eaafa3f9d093
EricLee <ericlee@EricLeede-MBP.frontierlocal.net>
2019-02-01 09:42:38 -0800

    Fix the wrong API LINK

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: `${process.env.REACT_APP_CRUNCHBASE_API_KEY}`,
+  apiKey: `${process.env.REACT_APP_GOOGLE_SHEET_API_KEY}`,
   discoveryDocs: 
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
   spreadsheetId: "1L5oU5W-QDg8wbaiQ2-QHE3hAPd7jQApVvubttkLNoOk"
