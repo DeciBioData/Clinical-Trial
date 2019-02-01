2d2bd3c387c5a1902e22ca3221c73f88d06f3cf4
EricLee <ericlee@EricLeede-MBP.frontierlocal.net>
2019-02-01 09:37:35 -0800

    Add .env

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyB9SO5sCfT2bfvnS8xaYmCLUuLwWswKaow",
+  apiKey: `${process.env.REACT_APP_CRUNCHBASE_API_KEY}`,
   discoveryDocs: 
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
   spreadsheetId: "1L5oU5W-QDg8wbaiQ2-QHE3hAPd7jQApVvubttkLNoOk"
